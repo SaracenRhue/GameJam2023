afe0bdc6734b4eda4ebe9bda3954ea9d7869965c
Ganko Pi <ganko-pi@outlook.de>
2023-11-03 21:10:02 +0100

    feat: add logical world generation

--- world.py
+++ world.py
@@ -1,3 +1,16 @@
+import random
+
 def get_layout(width: int, height: int, color_count: int) -> list: 
-    """Return a list of tupels replesenting right and bottom wall colors"""
-    print(f"Getting layout for a world of size {width}x{height}")
+    """Returns a list of 2-tuples representing right and bottom wall colors by integers"""
+    print(f"Getting layout for a world of size {width}x{height} with {color_count} colors")
+
+    result = []
+    for i in range(height):
+        row = []
+        for i in range(width):
+            right_wall = random.randrange(color_count)
+            bottom_wall = random.randrange(color_count)
+            row.append((right_wall, bottom_wall))
+        result.append(row)
+
+    return result
