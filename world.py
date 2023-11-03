bdc2379dca14f072cf3c2a6d30f5c8a441b5ed50
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2023-11-03 19:48:17 +0100

    init

---
+++ world.py
@@ -0,0 +1,3 @@
+def get_layout(width: int, height: int, color_count: int) -> list: 
+    """Return a list of tupels replesenting right and bottom wall colors"""
+    print(f"Getting layout for a world of size {width}x{height}")
