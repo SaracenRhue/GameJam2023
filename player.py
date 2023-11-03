b384e316c7af185a91cb401d63371185f5ae03ad
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2023-11-03 20:02:15 +0100

    u

---
+++ player.py
@@ -0,0 +1,11 @@
+def up() -> None:
+    """Move the player up one square."""    
+
+def down() -> None:
+    """Move the player down one square."""
+
+def left() -> None:
+    """Move the player left one square."""
+
+def right() -> None:
+    """Move the player right one square."""
