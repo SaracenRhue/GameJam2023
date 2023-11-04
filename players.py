0a6503c85515e615f01b6c10a053677049b54e93
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2023-11-04 09:08:41 +0100

    u

--- players.py
+++ players.py
@@ -22,32 +22,52 @@ class Player:
 
 
     def move_up(self, world):
-        if self.y_pos > 0 and world[self.y_pos - 1][self.x_pos][1] == self.color:
-            self.y_pos -= 1
-            print("Moved up")
-        else: 
-            print("Can't move up")
+        if self.y_pos > 0:
+            above_bottom_wall_color = world[self.y_pos - 1][self.x_pos][1]
+            print(f"Trying to move up. Above bottom wall color: {above_bottom_wall_color}, Player color: {self.color}")
+            if above_bottom_wall_color == self.color:
+                self.y_pos -= 1
+                print("Moved up")
+            else:
+                print("Can't move up - wall color does not match player color")
+        else:
+            print("Can't move up - at the top edge")
 
     def move_down(self, world):
-        if self.y_pos < len(world) - 1 and world[self.y_pos][self.x_pos][1] == self.color:
-            self.y_pos += 1
-            print("Moved down")
+        if self.y_pos < len(world) - 1:
+            current_bottom_wall_color = world[self.y_pos][self.x_pos][1]
+            print(f"Trying to move down. Current bottom wall color: {current_bottom_wall_color}, Player color: {self.color}")
+            if current_bottom_wall_color == self.color:
+                self.y_pos += 1
+                print("Moved down")
+            else:
+                print("Can't move down - wall color does not match player color")
         else:
-            print("Can't move down")
+            print("Can't move down - at the bottom edge")
 
     def move_left(self, world):
-        if self.x_pos > 0 and world[self.y_pos][self.x_pos - 1][0] == self.color:
-            self.x_pos -= 1
-            print("Moved left")
+        if self.x_pos > 0:
+            left_right_wall_color = world[self.y_pos][self.x_pos - 1][0]
+            print(f"Trying to move left. Left right wall color: {left_right_wall_color}, Player color: {self.color}")
+            if left_right_wall_color == self.color:
+                self.x_pos -= 1
+                print("Moved left")
+            else:
+                print("Can't move left - wall color does not match player color")
         else:
-            print("Can't move left")
+            print("Can't move left - at the left edge")
 
     def move_right(self, world):
-        if self.x_pos < len(world[0]) - 1 and world[self.y_pos][self.x_pos][0] == self.color:
-            self.x_pos += 1
-            print("Moved right")
+        if self.x_pos < len(world[0]) - 1:
+            current_right_wall_color = world[self.y_pos][self.x_pos][0]
+            print(f"Trying to move right. Current right wall color: {current_right_wall_color}, Player color: {self.color}")
+            if current_right_wall_color == self.color:
+                self.x_pos += 1
+                print("Moved right")
+            else:
+                print("Can't move right - wall color does not match player color")
         else:
-            print("Can't move right")
+            print("Can't move right - at the right edge")
 
 
 
