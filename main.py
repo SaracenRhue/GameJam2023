bdfbd630c8c09fcb5212a5875c7ee2cc5021bb97
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2023-11-04 11:40:10 +0100

    debouncig

--- main.py
+++ main.py
@@ -11,8 +11,6 @@ fps = 25
 square_size = 50  # Size of each square in pixels
 color_count = 3  
 
-
-
 # Set up the clock for a decent framerate
 clock = pygame.time.Clock()
 
@@ -25,39 +23,31 @@ def restart_game():
     
 # Initialize the game
 restart_game()
+
 # Adjust window size based on grid dimensions and square size
 window_height = (len(world) + 1) * square_size  # +1 to add space for the queue
 window_width = len(world[0]) * square_size
 
 window = pygame.display.set_mode((window_width, window_height))
 pygame.display.set_caption('Pygame Player Movement')
+
 running = True
+
 while running:
     # Handle events
     for event in pygame.event.get():
         if event.type == QUIT:
             running = False
-
-    # Get pressed keys
-    keys = pygame.key.get_pressed()
-
-    # Quit if 'Q' is pressed
-    if keys[K_q]:
-        running = False
-    
-    # Restart if 'R' is pressed
-    if keys[K_r]:
-        restart_game()
-
-    # Move the player with WASD
-    if keys[K_w]:
-        player.move_up(world)
-    if keys[K_s]:
-        player.move_down(world)
-    if keys[K_a]:
-        player.move_left(world)
-    if keys[K_d]:
-        player.move_right(world)
+        elif event.type == KEYDOWN:
+            # Use an if-elif block to handle a single key press event
+            if event.key == K_w:
+                player.move_up(world)
+            elif event.key == K_s:
+                player.move_down(world)
+            elif event.key == K_a:
+                player.move_left(world)
+            elif event.key == K_d:
+                player.move_right(world)
 
     # Draw the world and the player
     ui.draw_world(world, player, window, square_size)
