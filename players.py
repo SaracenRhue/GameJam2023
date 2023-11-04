f2d6e3131307387ab3bd9bffd5a54b2d3c38b59c
Ganko Pi <ganko-pi@outlook.de>
2023-11-04 13:23:11 +0100

    feat: color queue ensures possible path

--- players.py
+++ players.py
@@ -1,4 +1,5 @@
 import random
+import typing
 
 class Player:
     def __init__(self, world_layout, color_count, color_queue_length = 3):
@@ -9,7 +10,7 @@ class Player:
         self.color_count = color_count
         self.color_queue_length = color_queue_length
         self.color_queue = []
-        update_color_queue()
+        self.update_color_queue()
 
     def get_position(self):
         return (self.x_pos, self.y_pos)
@@ -27,12 +28,57 @@ class Player:
     def update_color(self):
         self.color = self.color_queue[0]
         self.color_queue.pop(0)
-        update_color_queue()
+        self.update_color_queue()
         print(f"Updated player color to {self.color} with color queue {self.color_queue}")
 
     def update_color_queue(self) -> None:
         while len(self.color_queue) < self.color_queue_length:
-            self.color_queue.append(random.randrange(self.color_count))
+            possible_next_colors_for_valid_paths = self.__possible_next_colors_for_valid_paths((self.x_pos, self.y_pos), -1)
+            if len(possible_next_colors_for_valid_paths) == 0:
+                print("You chose a wrong path and you are stuck")
+                return
+            next_color_index = random.randrange(len(possible_next_colors_for_valid_paths))
+            next_color = possible_next_colors_for_valid_paths[next_color_index]
+            self.color_queue.append(next_color)
+
+    def __possible_next_colors_for_valid_paths(self, current_position: typing.Tuple[int, int], color_queue_index: int) -> typing.Tuple[int]:
+        possible_next_colors = set()
+        top_wall_color = None
+        bottom_wall_color = None
+        left_wall_color = None
+        right_wall_color = None
+
+        if current_position[1] > 0:
+            top_wall_color = self.world_layout[current_position[1] - 1][current_position[0]][1]
+        if current_position[1] < len(self.world_layout) - 1:
+            bottom_wall_color = self.world_layout[current_position[1]][current_position[0]][1]
+        if current_position[0] > 0:
+            left_wall_color = self.world_layout[current_position[1]][current_position[0] - 1][0]
+        if current_position[0] < len(self.world_layout[0]) - 1:
+            right_wall_color = self.world_layout[current_position[1]][current_position[0]][0]
+
+        if color_queue_index >= len(self.color_queue):
+            possible_next_colors.add(top_wall_color)
+            possible_next_colors.add(bottom_wall_color)
+            possible_next_colors.add(left_wall_color)
+            possible_next_colors.add(right_wall_color)
+            possible_next_colors.discard(None)
+            return tuple(possible_next_colors)
+        
+        current_color = self.color
+        if color_queue_index >= 0:
+            current_color = self.color_queue[color_queue_index]
+
+        if top_wall_color == current_color:
+            possible_next_colors.update(self.__possible_next_colors_for_valid_paths((current_position[0], current_position[1] - 1), color_queue_index + 1))
+        if bottom_wall_color == current_color:
+            possible_next_colors.update(self.__possible_next_colors_for_valid_paths((current_position[0], current_position[1] + 1), color_queue_index + 1))
+        if left_wall_color == current_color:
+            possible_next_colors.update(self.__possible_next_colors_for_valid_paths((current_position[0] - 1, current_position[1]), color_queue_index + 1))
+        if right_wall_color == current_color:
+            possible_next_colors.update(self.__possible_next_colors_for_valid_paths((current_position[0] + 1, current_position[1]), color_queue_index + 1))
+
+        return tuple(possible_next_colors)
 
     def move_up(self, world):
         if self.y_pos > 0:
